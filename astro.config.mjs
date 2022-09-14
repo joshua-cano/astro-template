72b99db652c2bf851370ade001ea3d0ffc36b28c
Joshua Cano <joshua.cano18@gmail.com>
2022-09-14 13:34:39 -0500

    Updating header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
-import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), svelte()],
+  integrations: [sitemap()],
 })
